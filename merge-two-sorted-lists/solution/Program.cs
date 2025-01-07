e66714e9c29e9d557c8e6902f43b5c4ed6f41ed5
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-01-06 22:57:31 -0300

    init: remove-duplicates-from-sorted-array

---
+++ merge-two-sorted-lists/solution/Program.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        List<int> ordedList = [];
+
+        for(int n = 0; n < list1.Count(); n++ )
+        {
+            
+        }
+    }
+}
