09b77d1437b55e63dfd8bb071ff97e4ba044d941
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-01-18 23:24:50 -0300

    solved: find-the-index-of-the-first-occurrence-in-a-string

--- find-the-index-of-the-first-occurrence-in-a-string/solution/Program.cs
+++ find-the-index-of-the-first-occurrence-in-a-string/solution/Program.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        
+        int currentIndex = haystack.IndexOf(needle);
+        return currentIndex;
     }
 }
