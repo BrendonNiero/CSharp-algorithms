afc63f64083192f9709c8f369c20cb23d8475db7
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-01-24 22:03:03 -0300

    feat: init solution at plus one project

--- plus-one/solution/Program.cs
+++ plus-one/solution/Program.cs
@@ -1,6 +1,11 @@
 public class Solution {
     public int[] PlusOne(int[] digits) {
-        string nums = string.Join("", digits);
-        int total = int.Parse(nums) + 1;
+        int lastNumber = digits[digits.Length - 1];
+        lastNumber += 1;
+        int[] newArray = new int[digits.Length -1];
+        Array.Copy(digits, newArray, digits.Length -1);
+        int[] splitNumber = lastNumber.ToString().Select(c => int.Parse(c.ToString())).ToArray();
+        int[] updatedArray = newArray.Concat(splitNumber).ToArray();
+        return updatedArray;
     }
 }
