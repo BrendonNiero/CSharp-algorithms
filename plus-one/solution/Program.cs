e045d7b43626140f483c95589b5a3a499cc27ccb
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-01-25 22:55:59 -0300

    fix: solution at plus one

--- plus-one/solution/Program.cs
+++ plus-one/solution/Program.cs
@@ -1,11 +1,15 @@
 public class Solution {
     public int[] PlusOne(int[] digits) {
-        int lastNumber = digits[digits.Length - 1];
-        lastNumber += 1;
-        int[] newArray = new int[digits.Length -1];
-        Array.Copy(digits, newArray, digits.Length -1);
-        int[] splitNumber = lastNumber.ToString().Select(c => int.Parse(c.ToString())).ToArray();
-        int[] updatedArray = newArray.Concat(splitNumber).ToArray();
-        return updatedArray;
+        int n = digits.Length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+        return newDigits;
     }
 }
