61c88568df6856ab42c0f7c4510672e59eac6da2
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-01-27 22:08:47 -0300

    init: solution add-binary

--- difficult/easy/add-binary/solution/Program.cs
+++ difficult/easy/add-binary/solution/Program.cs
@@ -1,5 +1,9 @@
 public class Solution {
     public string AddBinary(string a, string b) {
-        
+        var firstValue = Convert.ToInt32(a, 2);
+        var secondValue = Convert.ToInt32(b, 2);
+        var result = firstValue + secondValue;
+        var binaryResult = Convert.ToString(result, 2);
+        return binaryResult;
     }
 }
