672eb57fe1d422a57b75b3932afda863657bd8cf
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-02-03 20:11:12 -0300

    feat: solved remove-duplicates-from-sorted-list

--- difficult/easy/remove-duplicates-from-sorted-list/solution/Program.cs
+++ difficult/easy/remove-duplicates-from-sorted-list/solution/Program.cs
@@ -1,5 +1,13 @@
 public class Solution {
     public ListNode DeleteDuplicates(ListNode head) {
-        
+        var cur = head;
+        while(cur != null && cur.next != null){
+            if(cur.next.val == cur.val){
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
     }
 }
