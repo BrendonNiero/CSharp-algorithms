eb8f0dd5a4588e846273ab140d1638b0cc5c3cf2
Brendon Niero <brendonsoftwaredeveloper@gmail.com>
2025-02-08 23:47:29 -0300

    init: init solution

--- difficult/easy/binary-tree-inorder-traversal/solution/Program.cs
+++ difficult/easy/binary-tree-inorder-traversal/solution/Program.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public IList<int> InorderTraversal(TreeNode root) {
-        
+        var res = new List<int>();
+        var stack = new List();
+        var cur = root;
     }
 }
